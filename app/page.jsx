057c74f733e705a3d97d8dbc8e41fe1bb573a56e
Carlos Perez <pcarlos132@yahoo.com>
2024-04-26 18:58:28 -0500

    added property card component

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
 
 const HomePage = () => {
   return (
     <>
-      <div className="text-3xl">HomePage</div>
-      <Link href="/properties">to properties</Link>
+      <Hero />
+      <InfoBoxes />
     </>
   );
 };
