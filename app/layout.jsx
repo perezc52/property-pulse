129e3a15c2dce793152a3363cb7d41c9020278ca
Carlos Perez <pcarlos132@yahoo.com>
2024-05-01 19:01:25 -0500

    auth provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,23 +1,26 @@
 import React from "react";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import AuthProvider from "@/components/AuthProvider";
 import "@/assets/styles/globals.css";
 
 export const metadata = {
   title: "Property Pulse | Find The Perfect Rental",
   description: "Find your dream rental property",
-  keywords: 'rental, find rentals, find properties',
+  keywords: "rental, find rentals, find properties",
 };
 
 const MainLayout = ({ children }) => {
   return (
-    <html>
-      <body>
-        <Navbar/>
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html>
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
