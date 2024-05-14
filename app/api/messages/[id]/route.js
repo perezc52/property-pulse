c5af8235308431dc9beb5975225bca736f4f587f
Carlos Perez <pcarlos132@yahoo.com>
2024-05-13 19:03:43 -0500

    messages

---
+++ app/api/messages/[id]/route.js
@@ -0,0 +1,46 @@
+import connectDB from "@/config/database";
+import Message from "@/models/Message";
+import { getSessionUser } from "@/utils/getSessionUser";
+
+export const dynamic = "force-dynamic";
+
+// PUT /api/messages/:id
+export const PUT = async (request, { params }) => {
+  try {
+    await connectDB();
+
+    const { id } = params;
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID is required", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
+    // Find message by id
+    const message = await Message.findById(id);
+
+    // Check if message exists
+    if (!message) return new Response("Message Not Found", { status: 404 });
+
+    // Verify ownership
+    if (message.recipient.toString() !== userId) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    // Update message to red/unread depending on the current status
+    message.read = !message.read;
+
+    // Save message
+    await message.save();
+
+    return new Response(JSON.stringify(message), {
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
